5963377fd6b7cd334827b69c0866042e43bcd2aa
GitLab User <glab@svmail.org>
2022-11-10 15:13:30 +0100

    v

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu! Another commit 10Nov #2";
+		return "Java web app - #5 10Nov";
 	}
 
 }
--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from Spring Boot + Tanzu! Another commit 10Nov #2", controller.index());
+        assertEquals("Java web app - #3 10Nov", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Spring Boot + Tanzu! Another commit 10Nov #2"));
+            .andExpect(content().string("Java web app - #3 10Nov"));
     }
 }
